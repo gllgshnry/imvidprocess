6fddf1a7d2d3236f12664d1a599d0547fc78dbf8
Henry Gallegos <gllgshnry@yahoo.com>
2015-01-16 17:27:30 -0800

    Added markdown and LaTex example. Visible when published.

--- Test.m
+++ Test.m
@@ -1,12 +1,20 @@
+%% Test m-file and its markdown
 % This is a test m-file to try and upload onto GitHub
 t = linspace(0,.5,500);
 f = 60;
 x = exp(-2*t).*sin(2*pi*f*t);
 plot(t,x), title('x vs. t'),xlabel('time'),ylabel('x')
 
+%% MATLAB and the Command Line
 % you can start MATLAB from the command line with the following parameters:
-% mlab -nosplash -nodesktop
-% mlab is the alias used to launch matlab on my computer as baam25
-% -nosplash prevents the splash page from showing when the program starts upload
-% -nodesktop prevents matlab from launching the full java program, only uses the
+% 
+% |mlab -nosplash -nodesktop|
+%
+% |mlab| is the alias used to launch matlab on my computer as baam25
+% |-nosplash| prevents the splash page from showing when the program starts upload
+% |-nodesktop| prevents matlab from launching the full java program, only uses the
 % engine to perform calculations, produce plots, etc.
+
+%% Test out LaTex equations
+% $$ y = y + \frac{sin(\omega*t)} {\omega*t} $$
+
